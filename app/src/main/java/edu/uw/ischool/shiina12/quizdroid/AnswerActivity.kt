5824177fe0731be6fa3dfa00fcfc063f67dc7e43
Shiina Sugioka <shiina12@uw.edu>
2023-10-31 15:05:51 -0700

    class declarations

--- app/src/main/java/edu/uw/ischool/shiina12/quizdroid/AnswerActivity.kt
+++ app/src/main/java/edu/uw/ischool/shiina12/quizdroid/AnswerActivity.kt
@@ -1,4 +1,12 @@
 package edu.uw.ischool.shiina12.quizdroid
 
-class AnswerActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AnswerActivity: AppCompatActivity()  {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_answer)
+        }
+    }
 }
--- app/src/main/java/edu/uw/ischool/shiina12/quizdroid/QuestionActivity.kt
+++ app/src/main/java/edu/uw/ischool/shiina12/quizdroid/QuestionActivity.kt
@@ -1,4 +1,11 @@
 package edu.uw.ischool.shiina12.quizdroid
 
-class QuestionActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class QuestionActivity: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
 }
